15a434f953c1483f0bad70cf57712d2ea8e89ef7
danielballan <daniel.b.allan@gmail.com>
2014-04-02 13:04:40 -0400

    Iteratively rotate to get better accuracy.

--- gaussians.py
+++ gaussians.py
@@ -1,8 +1,8 @@
 import numpy as np
-from scipy import ndimage
 from scipy.optimize import curve_fit
 from scipy.stats import linregress
 from pandas import Series
+from skimage import transform, filter
 from preprocessing import bigfish, threshold
 
 
@@ -39,16 +39,34 @@ def infer_angle_from_centers(x0):
         raise ValueError("Only {0} points were successfully fit.".format(len_x0))
     y = x0.index.values.astype(int)
     slope, intercept, r_value, p_value, std_err = linregress(x0, y)
-    angle = np.arctan(slope)
-    print slope, np.rad2deg(angle)
+    angle = np.pi/2 - np.arctan(slope)
+    print np.rad2deg(angle)
     return angle
 
 
-def analyze(image, guess_sigma=3.):
+class ConvergenceError(Exception):
+    pass
+
+
+def analyze(image, guess_sigma=3., gaussian_sigma=1., max_iterations=20):
     roi = bigfish(threshold(image))
-    blurred = ndimage.gaussian_filter(image[roi].astype('float'), 3)
+    image = image[roi].astype(float)
+    blurred = filter.gaussian_filter(image, gaussian_sigma)
     masked = np.where(threshold(blurred, -0.5),
                       blurred, np.zeros_like(blurred))
-    fits = fit_rows(blurred, guess_sigma)
-    angle = infer_angle_from_centers(fits[:, 2])
-    return np.rad2deg(angle)
+    preprocessed = blurred # currently, 'masked' is not used
+    fits = fit_rows(preprocessed, guess_sigma)
+    angle = np.rad2deg(infer_angle_from_centers(fits[:, 2]))
+    total_angle = angle
+    i = 0
+    while np.abs(angle) > 5:
+        preprocessed = transform.rotate(preprocessed, -angle)
+        fits = fit_rows(preprocessed, guess_sigma)
+        angle = np.rad2deg(infer_angle_from_centers(fits[:, 2]))
+        total_angle += angle
+        if i > max_iterations:
+            raise ConvergenceError(
+                "After {0} consecutive rotations, the image could not be "
+                "aligned to the vertical.".format(max_iterations))
+        i += 1
+    return np.mod(total_angle - 90, 180)
