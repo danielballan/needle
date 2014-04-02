9d469c4db7355793fb6cf33827c0902466973528
danielballan <daniel.b.allan@gmail.com>
2014-04-02 13:05:30 -0400

    Add tests of simulated data.

--- tests/test_wire.py
+++ tests/test_wire.py
@@ -1,34 +1,68 @@
 import unittest
 import os
 import numpy as np
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_allclose
+from skimage import draw, transform, filter
 import covariance
 import gaussians
 
 data_dir = os.path.join(os.path.dirname(covariance.__file__), 'tests', 'data')
 
+oblique = np.load(os.path.join(data_dir, 'oblique_frame.npy'))
+vertical = np.load(os.path.join(data_dir, 'vertical_frame.npy'))
+horizontal = np.load(os.path.join(data_dir, 'horizontal_frame.npy'))
+
+
+def sim_wire(angle, gaussian_sigma=1, noise_level=0, L=100):
+    "Draw a wire with blur and optional noise."
+    # Noise level should be from 0 to 1. 0.02 is reasonable.
+    shape = (L, L)
+
+    a = np.zeros(shape, dtype=np.uint8)
+    a[draw.ellipse(L//2, L//2, L//24, L//4)] = 100  # horizontal ellipse
+    a = filter.gaussian_filter(a, gaussian_sigma)
+    b = transform.rotate(a, angle)
+    b += noise_level*np.random.randn(*shape)
+    return b
+
 
 class BaseTestWire(object):
-    def setUp(self):
-        self.oblique = np.load(os.path.join(data_dir, 'oblique_frame.npy'))
-        self.vertical = np.load(os.path.join(data_dir, 'vertical_frame.npy'))
-        self.horizontal = np.load(os.path.join(data_dir, 'horizontal_frame.npy'))
-       
-    def test_oblique_wire(self):
-        assert_almost_equal(self.analyze(self.oblique), 53.392, decimal=0)
 
-    def test_vertical_wire(self):
-        assert_almost_equal(self.analyze(self.vertical), 91.484, decimal=0)
+    def compare(self, angle, atol, gaussian_sigma=1, noise_level=0, L=100):
+        actual = self.analyze(sim_wire(angle), **self.kwargs)
+        assert_allclose(actual, angle, atol=atol)
 
-    def test_horizontal_wire(self):
-        assert_almost_equal(self.analyze(self.horizontal), -177.515, decimal=0)
+#    def test_real_oblique_wire(self):
+#        assert_allclose(self.analyze(oblique, **self.kwargs), 53, atol=5)
+
+#    def test_real_vertical_wire(self):
+#        assert_allclose(self.analyze(vertical, **self.kwargs), 91, atol=5)
+
+#    def test_real_horizontal_wire(self):
+#        assert_allclose(self.analyze(horizontal, **self.kwargs), 3, atol=5)
+
+
+    def test_clean_wire_first_quadrant(self):
+        first_quadrant = [1, 10, 30, 50, 80, 89]
+        [self.compare(angle, atol=2) for angle in first_quadrant]
+
+    def test_noisy_wire_first_quadrant(self):
+        first_quadrant = [1, 10, 30, 50, 80, 89]
+        [self.compare(angle, noise_level=0.01, atol=2)
+         for angle in first_quadrant]
 
 
 class TestCovariance(BaseTestWire, unittest.TestCase):
-    def analyze(self, image):
-        return covariance.analyze(image)
+    def setUp(self):
+        self.kwargs = {}
+
+    def analyze(self, image, **kwargs):
+        return covariance.analyze(image, **kwargs)
 
 
 class TestGaussians(BaseTestWire, unittest.TestCase):
-    def analyze(self, image):
-        return gaussians.analyze(image)
+    def setUp(self):
+        self.kwargs = {'gaussian_sigma': 2}
+
+    def analyze(self, image, **kwargs):
+        return gaussians.analyze(image, **kwargs)
