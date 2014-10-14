44a529c832bedb76b5070644f357c5183644c105
danielballan <daniel.b.allan@gmail.com>
2014-10-14 16:39:06 -0400

    Add versioneer.

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 # This downloads and install setuptools if it is not installed.
 from setuptools import setup
+import versioneer
+
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'needle/_version.py'
+versioneer.versionfile_build = 'needle/_version.py'
+versioneer.tag_prefix = 'v'
+versioneer.parentdir_prefix = '.'
 
 setup_parameters = dict(
     name = "needle",
-    version = "0.0.1",
+    version = versioneer.get_version(),
+    cmdclass = versioneer.get_cmdclass(),
     description = "Track the orientation of elongated objects.",
     author = "Daniel Allan",
     author_email = "daniel.b.allan@jhu.edu",
